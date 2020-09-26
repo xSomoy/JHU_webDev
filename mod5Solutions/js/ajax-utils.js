79e652ecf351b4895b0619c6c4579f7576bdbd83
xSomoy <xsomoy@tutanota.com>
2020-09-26 15:34:47 +0600

    mod5 assingment done

--- mod5Solutions/js/ajax-utils.js
+++ mod5Solutions/js/ajax-utils.js
@@ -1,10 +1,10 @@
 (function (global) {
 
-// Set up a namespace for our utility
+
 var ajaxUtils = {};
 
 
-// Returns an HTTP request object
+
 function getRequestObject() {
   if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
@@ -20,7 +20,7 @@ function getRequestObject() {
 }
 
 
-// Makes an Ajax GET request to 'requestUrl'
+
 ajaxUtils.sendGetRequest = 
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
@@ -31,20 +31,17 @@ ajaxUtils.sendGetRequest =
                        isJsonResponse); 
       };
     request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+    request.send(null);
   };
 
 
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
+
 function handleResponse(request,
                         responseHandler,
                         isJsonResponse) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
 
-    // Default to isJsonResponse = true
     if (isJsonResponse == undefined) {
       isJsonResponse = true;
     }
@@ -59,7 +56,6 @@ function handleResponse(request,
 }
 
 
-// Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
 
 
